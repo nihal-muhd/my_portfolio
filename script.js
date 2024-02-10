426acb1631ec548a85bca162978d0560d7c4811e
Nihal Muhammed <nihalmuhammed7025@gmail.com>
2024-02-10 17:17:04 +0530

    finalising desin for each card. completed experiece

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const toggleMenu = () => {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+const readMore = () => {
+    const moreText = document.getElementsByClassName("more");
+    const dots = document.getElementsByClassName('dots')
+    console.log(dots);
+    for (let i = 0; i < moreText.length; i++) {
+        console.log(i, dots[i]);
+        moreText[i].classList.toggle("open");
+        dots[i].classList.toggle('close')
+    }
+}
